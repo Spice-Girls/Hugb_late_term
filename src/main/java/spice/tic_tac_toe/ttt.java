2da51418048d9a03126ad1801bd7cb14fba1856e
sveinbjorng <Sveinbjorn@SKYNET01>
2015-10-26 18:20:30 +0000

    Revert "Add constructor and tostring"
    This reverts commit 035b770a8d82ce0527f7790b9514a992ea58c7c2.

--- src/main/java/spice/tic_tac_toe/ttt.java
+++ src/main/java/spice/tic_tac_toe/ttt.java
@@ -1,22 +1,6 @@
 package spice.tic_tac_toe;
 
 public class ttt {
-	
-	private String[] board;
-	public ttt()
-	{
-		this.board = new String[9];
-	}
-	public String toString() { 
-		String board = "| --- | --- | --- |"
-					 + "|	  | 	|     |"
-					 + "| --- | --- | --- |"
-					 + "|	  | 	|     |"
-					 + "| --- | --- | --- |"
-					 + "|	  | 	|     |"
-					 + "| --- | --- | --- |";
-		return board;
-	}
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
