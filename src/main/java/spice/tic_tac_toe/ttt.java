035b770a8d82ce0527f7790b9514a992ea58c7c2
sveinbjorng <Sveinbjorn@SKYNET01>
2015-10-26 17:53:47 +0000

    Add constructor and tostring

--- src/main/java/spice/tic_tac_toe/ttt.java
+++ src/main/java/spice/tic_tac_toe/ttt.java
@@ -1,6 +1,22 @@
 package spice.tic_tac_toe;
 
 public class ttt {
+	
+	private String[] board;
+	public ttt()
+	{
+		this.board = new String[9];
+	}
+	public String toString() { 
+		String board = "| --- | --- | --- |"
+					 + "|	  | 	|     |"
+					 + "| --- | --- | --- |"
+					 + "|	  | 	|     |"
+					 + "| --- | --- | --- |"
+					 + "|	  | 	|     |"
+					 + "| --- | --- | --- |";
+		return board;
+	}
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
