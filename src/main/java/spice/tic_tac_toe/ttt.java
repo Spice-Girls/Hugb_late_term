4044b74dace68667a7a85b3e64289ecf5081e80a
steinnp <steiniell@gmail.com>
2015-10-27 21:08:03 +0000

    updated gradle scripts

--- src/main/java/spice/tic_tac_toe/ttt.java
+++ src/main/java/spice/tic_tac_toe/ttt.java
@@ -1,7 +1,12 @@
 package spice.tic_tac_toe;
 
+import java.util.Random;
+import org.apache.commons.lang3.StringUtils;
+
 public class ttt {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Random rand = new Random();
+        int test = rand.nextInt();
+        System.out.println(StringUtils.swapCase("Hello, world!") + test);
     }
 }
