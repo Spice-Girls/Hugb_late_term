07adf2f20652bc673659aada4edfc86932e09d3a
RoyalCookie <janus@clients-pool-1.nat.ru.is>
2015-10-31 19:22:11 +0000

    Implemented test functions for SetMove() and Implemented SetMove() in GameBoard.java and a helperfunction called toString() refactored and tested

--- src/main/java/spice/tic_tac_toe/GameBoard.java
+++ src/main/java/spice/tic_tac_toe/GameBoard.java
@@ -17,7 +17,30 @@ public class GameBoard {
     }
 
     public void setMove(int boxId, char player) {
+            boxId -= 1;
+            int x = boxId % 3;
+            int y = (boxId - (boxId % 3))/3;
 
+            if(boxId < 0 || boxId > 8) {
+                throw new IllegalArgumentException("boxId is not within the 1-9 range");
+            }
+
+            if(board[x][y] != initChar) {
+                throw new IllegalArgumentException("boxId refrences an already set box");
+            } else {
+                board[x][y] = player;
+            }
+        }
+
+
+    public String toString() {
+        String retString = "";
+        for (int i = 0; i < 9; i++) {
+            int x = i % 3;
+            int y = (i - (i % 3))/3;
+            retString += board[x][y];
+        }
+        return retString;
     }
 
 
