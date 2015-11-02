fc6578382edcdcae33c7961d5f9154b452df21ab
Skuli <skuliarnas@gmail.com>
2015-11-02 15:53:47 +0000

    fixed bugged test

--- src/test/java/spice/tic_tac_toe/GameBoardTest.java
+++ src/test/java/spice/tic_tac_toe/GameBoardTest.java
@@ -25,7 +25,7 @@ public class GameBoardTest {
         board.setMove(7);
 
         // Refrence String
-        String ref = "x  y  x  ";
+        String ref = "x  o  x  ";
 
         assertEquals(ref, board.toString());
     }
