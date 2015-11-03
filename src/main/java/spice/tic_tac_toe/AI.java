517ac2c479c62c3799bdb1616b55d60d9a4d494b
RoyalCookie <janus@clients-pool-1.nat.ru.is>
2015-11-03 13:34:24 +0000

    make it better

--- src/main/java/spice/tic_tac_toe/AI.java
+++ src/main/java/spice/tic_tac_toe/AI.java
@@ -3,11 +3,11 @@ package spice.tic_tac_toe;
 import java.util.Random;
 
 public class AI extends Player{
-	
+
 	public AI(char mark){
 		super(getCompName(),mark);
 	}
-	
+
 	private static String getCompName(){
 		Random r = new Random();
 		int name = r.nextInt(6);
@@ -30,9 +30,9 @@ public class AI extends Player{
 			return "IBM's Watson";
 		}
 	}
-	
+
 	public int getMove(GameBoard gameBoard){
-		
+
 		for(int i = 1; i < 10; i++){
 			if(gameBoard.isLegalMove(i)){
 				return i;
